863aeac07addf2d18e0e8fa23d3dc3ff7757e1ab
Carlos Montero <cdm.vfx@gmail.com>
2024-06-16 23:25:34 -0400

    Update dependencies

--- components/elements/ContactForm.tsx
+++ components/elements/ContactForm.tsx
@@ -1,18 +1,13 @@
 import { useState, useEffect, useCallback } from "react";
 import { FaCheck } from "react-icons/fa";
 import { MdEmail, MdPhone } from "react-icons/md";
-import { motion, AnimatePresence, useAnimation } from "framer-motion";
-import {
-  GoogleReCaptchaProvider,
-  useGoogleReCaptcha
-} from 'react-google-recaptcha-v3';
+import { useGoogleReCaptcha } from "react-google-recaptcha-v3";
 import { PopupButton } from "react-calendly";
 
 const ContactForm = () => {
+  const { executeRecaptcha } = useGoogleReCaptcha();
 
-	const { executeRecaptcha } = useGoogleReCaptcha();
-
-	const blankForm = {
+  const blankForm = {
     name: "",
     email: "",
     phone: "",
@@ -21,25 +16,27 @@ const ContactForm = () => {
 
   const [inputs, setInputs] = useState(blankForm);
 
-	const [token, setToken] = useState('');
+  const [token, setToken] = useState("");
 
   const [status, setStatus] = useState(false);
 
-  const changeInputs = (key: "name" | "email" | "phone" | "details", val: string) => {
+  const changeInputs = (
+    key: "name" | "email" | "phone" | "details",
+    val: string
+  ) => {
     let newInputs = { ...inputs };
     newInputs[key] = val;
     setInputs(newInputs);
   };
 
   const submitform = async () => {
+    setStatus(true);
 
-		setStatus(true);
+    let formdata: any = { ...inputs };
 
-		let formdata: any = {...inputs};
+    formdata.token = token;
 
-		formdata.token = token;
-
-		console.log('formdata: ', formdata);
+    console.log("formdata: ", formdata);
 
     const response = await fetch("/api/formsubmit", {
       method: "POST",
@@ -55,61 +52,76 @@ const ContactForm = () => {
       })
       .catch((err) => {
         console.log("Failed to submit form.", err);
-				setStatus(false);
+        setStatus(false);
       });
   };
 
-	const handleReCaptchaVerify = useCallback(
-		async () => {
-			
-			if (!executeRecaptcha) {
-				console.log('Execute recaptcha not yet available');
-				return;
-			}
+  const handleReCaptchaVerify = useCallback(async () => {
+    if (!executeRecaptcha) {
+      console.log("Execute recaptcha not yet available");
+      return;
+    }
 
-			const new_token = await executeRecaptcha('contactFormTest');
+    const new_token = await executeRecaptcha("contactFormTest");
 
-			setToken(new_token);
-		},
-		[executeRecaptcha],
-	);
+    setToken(new_token);
+  }, [executeRecaptcha]);
 
   useEffect(() => {
     handleReCaptchaVerify();
   }, [handleReCaptchaVerify]);
 
-
   return (
     <div className="pt-4 w-full space-y-8 text-center md:w-2/3 xl:w-1/3">
-			<PopupButton 
-					url="https://calendly.com/konceptdetailing" 
-				  text="SCHEDULE A DETAIL"
-					pageSettings={{
-						backgroundColor: 'ffffff',
-						hideEventTypeDetails: false,
-						hideGdprBanner: true,
-						hideLandingPageDetails: false,
-						primaryColor: '00a2ff',
-						textColor: '4d5055'
-					}}
-					styles={{letterSpacing: "2px", border: "1px solid white", cursor: "pointer", textAlign: "center", padding: "1rem" }}
-				/>
+      <PopupButton
+        url="https://calendly.com/konceptdetailing"
+        text="SCHEDULE A DETAIL"
+        pageSettings={{
+          backgroundColor: "ffffff",
+          hideEventTypeDetails: false,
+          hideGdprBanner: true,
+          hideLandingPageDetails: false,
+          primaryColor: "00a2ff",
+          textColor: "4d5055",
+        }}
+        styles={{
+          letterSpacing: "2px",
+          border: "1px solid white",
+          cursor: "pointer",
+          textAlign: "center",
+          padding: "1rem",
+        }}
+      />
       <div className="md:text-2xl md:leading-10 space-y-4 leading-8">
         <div className="space-y-4">
-          <p>If you would like to schedule a date and time that works best for you, please click the button above to see my availability!</p>
-					<p>If you have any questions, feel free to call, text or email me, or fill out the form below and I'll reach back out to you as soon as possible!</p>
+          <p>
+            If you would like to schedule a date and time that works
+            best for you, please click the button above to see my
+            availability!
+          </p>
+          <p>
+            If you have any questions, feel free to call, text or
+            email me, or fill out the form below and I'll reach back
+            out to you as soon as possible!
+          </p>
         </div>
         <div className="flex items-center justify-center space-x-2">
           <div>
             <MdPhone />
           </div>
-          <div><a href="tel:8136794397">813-679-4397</a></div>
+          <div>
+            <a href="tel:8136794397">813-679-4397</a>
+          </div>
         </div>
         <div className="flex items-center justify-center space-x-2">
           <div>
             <MdEmail />
           </div>
-          <div><a href="mailto:konceptdetailing@gmail.com">konceptdetailing@gmail.com</a></div>
+          <div>
+            <a href="mailto:konceptdetailing@gmail.com">
+              konceptdetailing@gmail.com
+            </a>
+          </div>
         </div>
       </div>
       <div>
@@ -165,11 +177,11 @@ const ContactForm = () => {
           disabled={status}
           className="bg-neutral-800 w-full py-2 px-4"
           type="button"
-          onClick={submitform}
-        >
+          onClick={submitform}>
           {status ? (
             <div className="flex flex-col justify-center items-center text-green-400 ">
-              <FaCheck /> Submitted! I'll get back to you as soon as possible!
+              <FaCheck /> Submitted! I'll get back to you as soon as
+              possible!
             </div>
           ) : (
             "Submit"
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,95 +1,101 @@
-import Head from 'next/head'
-import { useEffect, useState } from 'react'
-import About from '../components/About'
-import Contact from '../components/Contact'
-import FAQ from '../components/FAQ'
-import Gallery from '../components/Gallery'
-import Hero from '../components/Hero'
-import Navbar from '../components/Navbar'
-import Services from '../components/Services'
-import Testimonials from '../components/Testimonials'
+import Head from "next/head";
+import { useEffect, useState } from "react";
+import About from "../components/About";
+import Contact from "../components/Contact";
+import FAQ from "../components/FAQ";
+import Gallery from "../components/Gallery";
+import Hero from "../components/Hero";
+import Navbar from "../components/Navbar";
+import Services from "../components/Services";
+import Testimonials from "../components/Testimonials";
 
 export default function Home() {
-
-	
   const [offsetY, setOffsetY] = useState(0);
   const handleScroll = () => setOffsetY(window.scrollY);
 
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
-		
-		const parallaxbackground = document.querySelector('#parallaxbackground') as Element;
 
-		parallaxbackground.addEventListener('click', (event) => {  
-			event.preventDefault();  
-		});
+    const parallaxbackground = document.querySelector(
+      "#parallaxbackground"
+    ) as Element;
 
-		return () => window.removeEventListener("scroll", handleScroll);
+    parallaxbackground.addEventListener("click", (event) => {
+      event.preventDefault();
+    });
+
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
   return (
-    <div className=''>
+    <div className="">
       <Head>
         <title>Koncept Detailing - Cars, SUVs, Motorcycles</title>
-        <meta name="description" content="Former United States Marine based in Tampa, Florida with over 5 years of experience in auto detailing." />
+        <meta
+          name="description"
+          content="Former United States Marine based in Tampa, Florida with over 5 years of experience in auto detailing."
+        />
         <link rel="icon" href="/images/favicon.ico" />
-				<link rel="preconnect" href="https://fonts.googleapis.com" />
-				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-				<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;1,700&display=swap" rel="stylesheet" />
-			</Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;1,700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
 
-      <main className='relative overflow-hidden bg-[#111111]'>
-				<div
-					id="parallaxbackground" 
-					className="pointer-events-none z-10 opacity-10 lg:bg-[length:50%]"
-					style={{ 
-						width: "100%",
-						height: "100%",
-						position: "absolute",
-						backgroundImage: "url(../images/bg-crosssquares.svg)",
-						backgroundRepeat: "repeat",
-						backgroundPosition: "center top",
-						transform: `translateY(${offsetY * 0.2}px)`,
-					}}
-				></div>
-				<div
-					id="parallaxbackground" 
-					className="pointer-events-none z-10 opacity-30 lg:bg-[length:50%]"
-					style={{ 
-						width: "100%",
-						height: "100%",
-						position: "absolute",
-						backgroundImage: "url(../images/bg-rectangles.svg)",
-						backgroundRepeat: "repeat",
-						backgroundPosition: "center top",
-						transform: `translateY(${offsetY * 0.5}px)`,
-					}}
-				></div>
-				<div
-					id="parallaxbackground" 
-					className="pointer-events-none z-10 opacity-30 lg:bg-[length:50%]"
-					style={{ 
-						width: "100%",
-						height: "100%",
-						position: "absolute",
-						backgroundImage: "url(../images/bg-tinysquares.svg)",
-						backgroundRepeat: "repeat",
-						backgroundPosition: "center top",
-						transform: `translateY(${offsetY * 0.7}px)`,
-					}}
-				></div>
-				<Navbar />
-				<Hero />
-				<Services />
-				<About />
-				<Testimonials  />
-				<Gallery />
-				<FAQ />
-				<Contact />
+      <main className="relative overflow-hidden bg-[#111111]">
+        <div
+          id="parallaxbackground"
+          className="pointer-events-none z-10 opacity-10 lg:bg-[length:50%]"
+          style={{
+            width: "100%",
+            height: "100%",
+            position: "absolute",
+            backgroundImage: "url(../images/bg-crosssquares.svg)",
+            backgroundRepeat: "repeat",
+            backgroundPosition: "center top",
+            transform: `translateY(${offsetY * 0.2}px)`,
+          }}></div>
+        <div
+          id="parallaxbackground"
+          className="pointer-events-none z-10 opacity-30 lg:bg-[length:50%]"
+          style={{
+            width: "100%",
+            height: "100%",
+            position: "absolute",
+            backgroundImage: "url(../images/bg-rectangles.svg)",
+            backgroundRepeat: "repeat",
+            backgroundPosition: "center top",
+            transform: `translateY(${offsetY * 0.5}px)`,
+          }}></div>
+        <div
+          id="parallaxbackground"
+          className="pointer-events-none z-10 opacity-30 lg:bg-[length:50%]"
+          style={{
+            width: "100%",
+            height: "100%",
+            position: "absolute",
+            backgroundImage: "url(../images/bg-tinysquares.svg)",
+            backgroundRepeat: "repeat",
+            backgroundPosition: "center top",
+            transform: `translateY(${offsetY * 0.7}px)`,
+          }}></div>
+        <Navbar />
+        <Hero />
+        <Services />
+        <About />
+        <Testimonials />
+        <Gallery />
+        <FAQ />
+        <Contact />
       </main>
 
-      <footer>
-      </footer>
+      <footer></footer>
     </div>
-  )
+  );
 }
